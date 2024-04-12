213289b9a09c17379bbfc38a39e02c6842b2c572
Meet Mangukiya <meet@flamy.dev>
2024-04-12 19:09:35 +0530

    CoWFeeModule

---
+++ src/CoWFeeModule.sol
@@ -0,0 +1,245 @@
+pragma solidity 0.8.25;
+
+interface ISafe {
+    enum Operation {
+        Call,
+        DelegateCall
+    }
+
+    function execTransactionFromModuleReturnData(address to, uint256 value, bytes memory data, Operation operation)
+        external
+        returns (bool success, bytes memory returnData);
+
+    function enableModule(address module) external;
+}
+
+interface IGPv2Settlement {
+    struct OrderData {
+        address sellToken;
+        address buyToken;
+        address receiver;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        uint32 validTo;
+        bytes32 appData;
+        uint256 feeAmount;
+        bytes32 kind;
+        bool partiallyFillable;
+        bytes32 sellTokenBalance;
+        bytes32 buyTokenBalance;
+    }
+
+    struct TradeData {
+        uint256 sellTokenIndex;
+        uint256 buyTokenIndex;
+        address receiver;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        uint32 validTo;
+        bytes32 appData;
+        uint256 feeAmount;
+        uint256 flags;
+        uint256 executedAmount;
+        bytes signature;
+    }
+
+    struct InteractionData {
+        address to;
+        uint256 value;
+        bytes callData;
+    }
+
+    function settle(
+        address[] memory tokens,
+        uint256[] memory clearingPrices,
+        TradeData[] memory trades,
+        InteractionData[][3] memory interactions
+    ) external;
+
+    function setPreSignature(bytes calldata orderUid, bool signed) external;
+
+    function domainSeparator() external view returns (bytes32);
+}
+
+interface IERC20 {
+    function approve(address, uint256) external;
+}
+
+IGPv2Settlement constant settlement = IGPv2Settlement(0x9008D19f58AAbD9eD0D60971565AA8510560ab41);
+address constant vaultRelayer = 0xC92E8bdf79f0507f65a392b0ab4667716BFE0110;
+bytes32 constant ORDER_TYPE_HASH = hex"d5a25ba2e97094ad7d83dc28a6572da797d6b3e7fc6663bd93efb789fc17e489";
+bytes32 constant ERC20_BALANCE_HASH = keccak256("erc20");
+bytes32 constant SELL_KIND_HASH = keccak256("sell");
+
+contract CoWFeeModule {
+    error OnlyKeeper();
+
+    ISafe immutable receiver;
+    address immutable toToken;
+    address immutable keeper;
+    bytes32 immutable domainSeparator;
+
+    struct Revocation {
+        address token;
+        address spender;
+    }
+
+    struct SwapToken {
+        address token;
+        uint256 sellAmount;
+    }
+
+    constructor(address _receiver, address _toToken, address _keeper) {
+        receiver = ISafe(_receiver);
+        toToken = _toToken;
+        keeper = _keeper;
+        domainSeparator = settlement.domainSeparator();
+    }
+
+    function approve(address[] memory _tokens) external onlyKeeper {
+        IGPv2Settlement.InteractionData[] memory approveInteractions =
+            new IGPv2Settlement.InteractionData[](_tokens.length);
+
+        for (uint256 i = 0; i < _tokens.length;) {
+            address token = _tokens[i];
+            approveInteractions[i] = IGPv2Settlement.InteractionData({
+                to: token,
+                value: 0,
+                callData: abi.encodeCall(IERC20.approve, (vaultRelayer, type(uint256).max))
+            });
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        _execInteractions(approveInteractions);
+    }
+
+    function revoke(Revocation[] memory _revocations) external onlyKeeper {
+        IGPv2Settlement.InteractionData[] memory revokeInteractions =
+            new IGPv2Settlement.InteractionData[](_revocations.length);
+        for (uint256 i = 0; i < _revocations.length;) {
+            Revocation memory revocation = _revocations[i];
+            revokeInteractions[i] = IGPv2Settlement.InteractionData({
+                to: revocation.token,
+                value: 0,
+                callData: abi.encodeCall(IERC20.approve, (revocation.spender, 0))
+            });
+
+            unchecked {
+                ++i;
+            }
+        }
+        _execInteractions(revokeInteractions);
+    }
+
+    function drip(SwapToken[] memory _swapTokens) external onlyKeeper {
+        IGPv2Settlement.InteractionData[] memory dripInteractions =
+            new IGPv2Settlement.InteractionData[](_swapTokens.length);
+
+        IGPv2Settlement.OrderData memory order = IGPv2Settlement.OrderData({
+            sellToken: address(0),
+            buyToken: toToken,
+            receiver: address(receiver),
+            sellAmount: 0,
+            buyAmount: 1,
+            validTo: nextValidTo(),
+            appData: "CoWFeeModule",
+            feeAmount: 0,
+            kind: SELL_KIND_HASH,
+            partiallyFillable: true,
+            sellTokenBalance: ERC20_BALANCE_HASH,
+            buyTokenBalance: ERC20_BALANCE_HASH
+        });
+
+        for (uint256 i = 0; i < dripInteractions.length;) {
+            SwapToken memory swapToken = _swapTokens[i];
+            order.sellToken = swapToken.token;
+            order.sellAmount = swapToken.sellAmount;
+            bytes memory preSignature = _computePreSignature(order);
+
+            dripInteractions[i] = IGPv2Settlement.InteractionData({
+                to: address(settlement),
+                value: 0,
+                callData: abi.encodeCall(IGPv2Settlement.setPreSignature, (preSignature, true))
+            });
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        _execInteractions(dripInteractions);
+    }
+
+    function nextValidTo() public view returns (uint32) {
+        return uint32(block.timestamp + 365 days) % 365 days;
+    }
+
+    function _execFromModule(address _to, bytes memory _cd) internal returns (bytes memory) {
+        (bool success, bytes memory returnData) =
+            receiver.execTransactionFromModuleReturnData(_to, 0, _cd, ISafe.Operation.Call);
+        if (!success) {
+            assembly ("memory-safe") {
+                /// @solidity-
+                revert(add(returnData, 0x20), mload(returnData))
+            }
+        }
+        return returnData;
+    }
+
+    function _execInteractions(IGPv2Settlement.InteractionData[] memory _interactions) internal {
+        address[] memory tokens = new address[](0);
+        uint256[] memory clearingPrices = new uint256[](0);
+        IGPv2Settlement.TradeData[] memory trades = new IGPv2Settlement.TradeData[](0);
+        IGPv2Settlement.InteractionData[][3] memory interactions =
+            [new IGPv2Settlement.InteractionData[](0), _interactions, new IGPv2Settlement.InteractionData[](0)];
+        bytes memory cd = abi.encodeCall(IGPv2Settlement.settle, (tokens, clearingPrices, trades, interactions));
+        _execFromModule(address(settlement), cd);
+    }
+
+    function _computePreSignature(IGPv2Settlement.OrderData memory order) internal view returns (bytes memory) {
+        bytes32 orderDigest = _computeOrderHash(order);
+        return abi.encodePacked(orderDigest, address(settlement), order.validTo);
+    }
+
+    function _computeOrderHash(IGPv2Settlement.OrderData memory order) internal view returns (bytes32 orderDigest) {
+        bytes32 structHash;
+
+        // NOTE: Compute the EIP-712 order struct hash in place. As suggested
+        // in the EIP proposal, noting that the order struct has 10 fields, and
+        // including the type hash `(12 + 1) * 32 = 416` bytes to hash.
+        // <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#rationale-for-encodedata>
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let dataStart := sub(order, 32)
+            let temp := mload(dataStart)
+            mstore(dataStart, ORDER_TYPE_HASH)
+            structHash := keccak256(dataStart, 416)
+            mstore(dataStart, temp)
+        }
+
+        bytes32 domainSeparator_ = domainSeparator;
+        // NOTE: Now that we have the struct hash, compute the EIP-712 signing
+        // hash using scratch memory past the free memory pointer. The signing
+        // hash is computed from `"\x19\x01" || domainSeparator || structHash`.
+        // <https://docs.soliditylang.org/en/v0.7.6/internals/layout_in_memory.html#layout-in-memory>
+        // <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#specification>
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let freeMemoryPointer := mload(0x40)
+            mstore(freeMemoryPointer, "\x19\x01")
+            mstore(add(freeMemoryPointer, 2), domainSeparator_)
+            mstore(add(freeMemoryPointer, 34), structHash)
+            orderDigest := keccak256(freeMemoryPointer, 66)
+        }
+    }
+
+    modifier onlyKeeper() {
+        if (msg.sender != keeper) {
+            revert OnlyKeeper();
+        }
+        _;
+    }
+}
---
+++ test/CoWFeeModule.t.sol
@@ -0,0 +1,176 @@
+import {
+    CoWFeeModule,
+    ISafe,
+    settlement,
+    vaultRelayer,
+    IGPv2Settlement,
+    ERC20_BALANCE_HASH,
+    SELL_KIND_HASH,
+    ORDER_TYPE_HASH
+} from "src/CoWFeeModule.sol";
+import { Test, Vm } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+
+contract MockERC20 {
+    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
+
+    function approve(address to, uint256 amt) external {
+        allowance[msg.sender][to] = amt;
+    }
+
+    function transfer(address to, uint256 amt) external {
+        balanceOf[msg.sender] -= amt;
+        balanceOf[to] += amt;
+    }
+}
+
+contract CoWFeeModuleTest is Test {
+    address keeper = makeAddr("keeper");
+    CoWFeeModule module;
+    MockERC20 mockToken;
+    address targetSafe = 0x423cEc87f19F0778f549846e0801ee267a917935;
+    address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function setUp() external {
+        module = new CoWFeeModule(targetSafe, WETH, keeper);
+        mockToken = new MockERC20();
+
+        vm.label(address(settlement), "settlement");
+        vm.label(address(vaultRelayer), "vaultRelayer");
+        vm.label(targetSafe, "targetSafe");
+        vm.label(address(module), "module");
+
+        vm.prank(targetSafe);
+        ISafe(targetSafe).enableModule(address(module));
+    }
+
+    function testAuth() external {
+        address[] memory tokens = new address[](0);
+        vm.expectRevert(CoWFeeModule.OnlyKeeper.selector);
+        module.approve(tokens);
+
+        CoWFeeModule.Revocation[] memory revocations = new CoWFeeModule.Revocation[](0);
+        vm.expectRevert(CoWFeeModule.OnlyKeeper.selector);
+        module.revoke(revocations);
+
+        CoWFeeModule.SwapToken[] memory swapTokens = new CoWFeeModule.SwapToken[](0);
+        vm.expectRevert(CoWFeeModule.OnlyKeeper.selector);
+        module.drip(swapTokens);
+    }
+
+    function testApprove() external {
+        uint256 currentAllowance = mockToken.allowance(address(settlement), address(vaultRelayer));
+        assertEq(currentAllowance, 0, "current allowance not 0");
+
+        address[] memory tokens = new address[](1);
+        tokens[0] = address(mockToken);
+        vm.prank(keeper);
+        module.approve(tokens);
+
+        uint256 newAllowance = mockToken.allowance(address(settlement), address(vaultRelayer));
+        assertEq(newAllowance, type(uint256).max, "allowance not max");
+    }
+
+    function testRevoke() external {
+        address[] memory tokens = new address[](1);
+        tokens[0] = address(mockToken);
+        vm.prank(keeper);
+        module.approve(tokens);
+
+        uint256 currentAllowance = mockToken.allowance(address(settlement), address(vaultRelayer));
+        assertEq(currentAllowance, type(uint256).max, "current allowance not max");
+
+        CoWFeeModule.Revocation[] memory revocations = new CoWFeeModule.Revocation[](1);
+        revocations[0] = CoWFeeModule.Revocation({ token: address(mockToken), spender: address(vaultRelayer) });
+        vm.prank(keeper);
+        module.revoke(revocations);
+
+        uint256 newAllowance = mockToken.allowance(address(settlement), address(vaultRelayer));
+        assertEq(newAllowance, 0, "allowance not 0");
+    }
+
+    event PreSignature(address indexed owner, bytes orderUid, bool signed);
+
+    function testDrip() external {
+        deal(address(mockToken), address(settlement), 100 ether);
+
+        uint32 nextValidTo = module.nextValidTo();
+        bytes32 orderHash = _computeOrderHash(
+            IGPv2Settlement.OrderData({
+                sellToken: address(mockToken),
+                buyToken: WETH,
+                receiver: targetSafe,
+                sellAmount: 100 ether,
+                buyAmount: 1,
+                validTo: nextValidTo,
+                appData: "CoWFeeModule",
+                feeAmount: 0,
+                kind: SELL_KIND_HASH,
+                partiallyFillable: true,
+                sellTokenBalance: ERC20_BALANCE_HASH,
+                buyTokenBalance: ERC20_BALANCE_HASH
+            })
+        );
+        bytes memory preSignature = abi.encodePacked(orderHash, address(settlement), nextValidTo);
+
+        CoWFeeModule.SwapToken[] memory swapTokens = new CoWFeeModule.SwapToken[](1);
+        swapTokens[0] = CoWFeeModule.SwapToken({ token: address(mockToken), sellAmount: 100 ether });
+
+        vm.recordLogs();
+        vm.prank(keeper);
+        module.drip(swapTokens);
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+
+        bool found = false;
+        address owner;
+        bytes memory orderUid;
+        bool signed;
+        for (uint256 i = 0; i < logs.length; i++) {
+            if (logs[i].topics.length > 0 && logs[i].topics[0] == PreSignature.selector) {
+                owner = address(uint160(uint256(logs[i].topics[1])));
+                (orderUid, signed) = abi.decode(logs[i].data, (bytes, bool));
+                found = true;
+                break;
+            }
+        }
+
+        assertTrue(found, "PreSignature not found");
+
+        assertEq(owner, address(settlement), "owner not settlement");
+        assertEq(signed, true, "not signed");
+        assertEq(orderUid, preSignature, "orderUid not correct");
+    }
+
+    function _computeOrderHash(IGPv2Settlement.OrderData memory order) internal view returns (bytes32 orderDigest) {
+        bytes32 structHash;
+
+        // NOTE: Compute the EIP-712 order struct hash in place. As suggested
+        // in the EIP proposal, noting that the order struct has 10 fields, and
+        // including the type hash `(12 + 1) * 32 = 416` bytes to hash.
+        // <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#rationale-for-encodedata>
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let dataStart := sub(order, 32)
+            let temp := mload(dataStart)
+            mstore(dataStart, ORDER_TYPE_HASH)
+            structHash := keccak256(dataStart, 416)
+            mstore(dataStart, temp)
+        }
+
+        bytes32 domainSeparator_ = settlement.domainSeparator();
+        // NOTE: Now that we have the struct hash, compute the EIP-712 signing
+        // hash using scratch memory past the free memory pointer. The signing
+        // hash is computed from `"\x19\x01" || domainSeparator || structHash`.
+        // <https://docs.soliditylang.org/en/v0.7.6/internals/layout_in_memory.html#layout-in-memory>
+        // <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#specification>
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let freeMemoryPointer := mload(0x40)
+            mstore(freeMemoryPointer, "\x19\x01")
+            mstore(add(freeMemoryPointer, 2), domainSeparator_)
+            mstore(add(freeMemoryPointer, 34), structHash)
+            orderDigest := keccak256(freeMemoryPointer, 66)
+        }
+    }
+}
