f1d016a4ee93b6d5c1ad807702979db81487c8ce
Meet Mangukiya <meet@flamy.dev>
2024-04-23 18:58:49 +0530

    refactor: use env vars in script for config
    Closes #6

--- script/COWFeeModule.s.sol
+++ script/COWFeeModule.s.sol
@@ -3,14 +3,19 @@ import { COWFeeModule, ISafe } from "src/COWFeeModule.sol";
 
 contract COWFeeModuleDeployScript is Script {
     function run() external {
-        address targetSafe = 0x423cEc87f19F0778f549846e0801ee267a917935;
-        address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        address keeper = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+        address receiver = vm.envAddress("RECEIVER");
+        address toToken = vm.envAddress("TO_TOKEN");
+        address keeper = vm.envAddress("KEEPER");
+        bytes32 appData = vm.envBytes32("APP_DATA");
+        bool shouldEnableModule = vm.envBool("SHOULD_ENABLE_MODULE");
 
-        bytes32 appData = 0xf3ccd10db03031ba3b5fa16bbd60cc86a9701a1be4928578f4fa24870b7f3515;
         vm.broadcast();
-        COWFeeModule module = new COWFeeModule(targetSafe, WETH, keeper, appData);
-        vm.broadcast(targetSafe);
-        ISafe(targetSafe).enableModule(address(module));
+        COWFeeModule module = new COWFeeModule(receiver, toToken, keeper, appData);
+
+        // only useful for local anvil setup
+        if (shouldEnableModule) {
+            vm.broadcast(receiver);
+            ISafe(receiver).enableModule(address(module));
+        }
     }
 }
